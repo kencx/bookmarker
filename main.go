6c52aa1d971c093b9f4dae36fb8cb41aede6acd9
kencx <kennethcheo1@gmail.com>
2022-01-12 01:19:18 +0800

    test: complete tests for crud
    minor feat: add db directory

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -12,8 +13,14 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	driver   = "sqlite3"
+	dbparent = "bookmarker"
+	filename = "bm.db"
+)
+
 type Bookmark struct {
-	id   int64
+	id   int
 	name string
 	url  string
 }
@@ -23,9 +30,9 @@ type Storage struct {
 }
 
 func newDB(path string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", path)
+	db, err := sql.Open(driver, path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to establish connection: %v", err)
+		return nil, fmt.Errorf("failed to establish connection: %w", err)
 	}
 	if err = createTable(db); err != nil {
 		return nil, err
@@ -48,42 +55,40 @@ func createTable(db *sql.DB) error {
 	return nil
 }
 
-func (s *Storage) AddBookmark(b *Bookmark) (int64, error) {
+func (s *Storage) AddBookmark(b *Bookmark) (int, error) {
 
 	stmt, err := s.db.Prepare("INSERT INTO bookmarks (name, url) VALUES (?, ?)")
 	if err != nil {
-		return -1, fmt.Errorf("prepare stmt failed: %v", err)
+		return -1, fmt.Errorf("prepare stmt failed: %w", err)
 	}
 	defer stmt.Close()
 
 	res, err := stmt.Exec(b.name, b.url)
 	if err != nil {
-		return -1, fmt.Errorf("failed to add row: %v", err)
+		return -1, fmt.Errorf("failed to add row: %w", err)
 	}
 
 	lastId, err := res.LastInsertId()
 	if err != nil {
-		return -1, fmt.Errorf("failed to get lastId: %v", err)
+		return -1, fmt.Errorf("failed to get lastId: %w", err)
 	}
-	b.id = lastId
+	b.id = int(lastId)
 	log.Printf("bookmark %d added successfully", b.id)
-	return lastId, nil
+	return int(lastId), nil
 }
 
-func (s *Storage) GetBookmarkById(id int64) (*Bookmark, error) {
+func (s *Storage) GetBookmarkById(id int) (Bookmark, error) {
 
-	stmt, err := s.db.Prepare("SELECT (name, url) FROM bookmarks WHERE id = ?")
+	stmt, err := s.db.Prepare("SELECT id, name, url FROM bookmarks WHERE id = ?")
 	if err != nil {
-		return nil, fmt.Errorf("prepare stmt failed: %v", err)
+		return Bookmark{}, fmt.Errorf("prepare stmt failed: %w", err)
 	}
 	defer stmt.Close()
 
-	var b *Bookmark
-	if err = stmt.QueryRow(id).Scan(&b.name, &b.url); err != nil {
-		if err == sql.ErrNoRows {
-			// no error
-		} else {
-			return nil, fmt.Errorf("failed to query row: %v", err)
+	var b Bookmark
+	if err = stmt.QueryRow(id).Scan(&b.id, &b.name, &b.url); err != nil {
+		if err != sql.ErrNoRows {
+			return Bookmark{}, fmt.Errorf("failed to query row: %w", err)
 		}
 	}
 	return b, nil
@@ -93,73 +98,74 @@ func (s *Storage) GetAllBookmarks() ([]Bookmark, error) {
 
 	stmt, err := s.db.Prepare("SELECT name, url FROM bookmarks")
 	if err != nil {
-		return nil, fmt.Errorf("prepare stmt failed: %v", err)
+		return nil, fmt.Errorf("prepare stmt failed: %w", err)
 	}
 	defer stmt.Close()
 
-	var bList []Bookmark
 	rows, err := stmt.Query()
 	if err != nil {
-		return nil, fmt.Errorf("failed to query row: %v", err)
+		return nil, fmt.Errorf("failed to query row: %w", err)
 	}
 	defer rows.Close()
 
+	var bList []Bookmark
 	for rows.Next() {
 		var b Bookmark
 		if err = rows.Scan(&b.name, &b.url); err != nil {
-			return nil, fmt.Errorf("failed to scan row: %v", err)
+			return nil, fmt.Errorf("failed to scan row: %w", err)
 		}
 		bList = append(bList, b)
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, fmt.Errorf("failed to scan rows: %v", err)
+		return nil, fmt.Errorf("failed to scan rows: %w", err)
 	}
 	return bList, nil
 }
 
-func (s *Storage) DeleteBookmark(id int64) error {
+func (s *Storage) DeleteBookmark(id int) (int64, error) {
 
 	stmt, err := s.db.Prepare("DELETE FROM bookmarks WHERE id = ?")
 	if err != nil {
-		return fmt.Errorf("prepare stmt failed: %v", err)
+		return -1, fmt.Errorf("prepare stmt failed: %w", err)
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(id)
+	res, err := stmt.Exec(id)
 	if err != nil {
-		return fmt.Errorf("failed to delete row: %v", err)
+		return -1, fmt.Errorf("failed to delete row: %w", err)
 	}
-	log.Printf("bookmark %d deleted successfully", id)
-	return nil
+	log.Printf("bookmark deleted successfully")
+	return res.RowsAffected()
 }
 
-func (s *Storage) DeleteAllBookmarks() error {
-	_, err := s.db.Exec("DELETE FROM bookmarks")
+func (s *Storage) DeleteAllBookmarks() (int64, error) {
+	res, err := s.db.Exec("DELETE FROM bookmarks")
 	if err != nil {
-		return fmt.Errorf("failed to drop bookmarks: %v", err)
+		return -1, fmt.Errorf("failed to delete all bookmarks: %w", err)
 	}
 	log.Printf("dropped bookmarks table successfully")
-	return nil
+	return res.RowsAffected()
 }
 
-func (s *Storage) UpdateBookmark(id int64, b *Bookmark) error {
+func (s *Storage) UpdateBookmark(id int, b *Bookmark) (int64, error) {
 
 	stmt, err := s.db.Prepare("UPDATE bookmarks SET name = ?, url = ? WHERE id = ?")
 	if err != nil {
-		return fmt.Errorf("prepare stmt failed: %v", err)
+		return -1, fmt.Errorf("prepare stmt failed: %w", err)
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(b.name, b.url, id)
+	res, err := stmt.Exec(b.name, b.url, id)
 	if err != nil {
-		return fmt.Errorf("failed to update row: %v", err)
+		return -1, fmt.Errorf("failed to update row: %w", err)
 	}
 	log.Printf("bookmark %d updated successfully", id)
-	return nil
+	return res.RowsAffected()
 }
 
-func (b *Bookmark) openBookmark() {
+// from https://gist.github.com/hyg/9c4afcd91fe24316cbf0
+func (b *Bookmark) OpenBookmark() error {
 	url := b.url
 	var err error
 
@@ -171,36 +177,69 @@ func (b *Bookmark) openBookmark() {
 	case "darwin":
 		err = exec.Command("open", url).Start()
 	default:
-		err = fmt.Errorf("unsupported platform")
+		err = fmt.Errorf("unsupported platform") // replace with defined error
 	}
 	if err != nil {
-		log.Fatalf("failed to open url: %v", err)
+		return fmt.Errorf("failed to open url: %w", err)
 	}
+	return nil
 }
 
-// from https://gist.github.com/hyg/9c4afcd91fe24316cbf0
-func GetDBDir() (string, error) {
+func GetBaseDir() (string, error) {
 	var err error
-	var fullpath string
+	var path string
 
 	switch runtime.GOOS {
 	case "windows":
-		fullpath = os.Getenv("APPDATA")
+		path = os.Getenv("APPDATA")
 	case "linux":
-		fullpath = os.Getenv("XDG_DATA_HOME")
-		if fullpath == "" {
+		// check config for custom dir
+		path = os.Getenv("XDG_DATA_HOME")
+		if path == "" {
 			homepath := os.Getenv("HOME")
-			fullpath = filepath.Join(homepath, ".local", "share")
+			path = filepath.Join(homepath, ".local", "share")
 		}
 	default:
 		err = fmt.Errorf("unsupported platform")
 	}
+	return path, err
+}
 
-	return fullpath, err
+func (s *Storage) importBookmarksJSON(path string) {
+
+}
+
+func (s *Storage) importBookmarksHTML(path string) {
+
+}
+
+// export to json, html, md
+func (s *Storage) exportBookmarksJSON(path string) {
+
 }
 
 func main() {
-	db, err := newDB("./bm.db")
+
+	// get config file
+
+	basedir, err := GetBaseDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbpath := filepath.Join(basedir, dbparent)
+	fullpath := filepath.Join(dbpath, filename)
+
+	_, err = os.Stat(fullpath)
+	if errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll(dbpath, 0755)
+		if err != nil {
+			log.Fatalf("Error creating parent dir: %v", err)
+		}
+	} else {
+		log.Fatalf("Error determining file structure: %v", err)
+	}
+
+	db, err := newDB(fullpath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -210,12 +249,14 @@ func main() {
 	if err = s.db.Ping(); err != nil {
 		log.Fatal(err)
 	}
-
 	log.Println("database connection successful")
 
+	// commands
+
+	// checking seeded data is cleared
 	bList, err := s.GetAllBookmarks()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(len(bList))
+	fmt.Println("Number of active rows: ", len(bList))
 }
--- main_test.go
+++ main_test.go
@@ -10,7 +10,12 @@ const (
 	path = "./bm.db"
 )
 
-var s *Storage
+var (
+	s             *Storage
+	testBookmark  = Bookmark{name: "google", url: "google.com"}
+	testBookmark2 = Bookmark{name: "reddit", url: "reddit.com"}
+	testList      = []Bookmark{testBookmark, testBookmark2}
+)
 
 func TestMain(m *testing.M) {
 
@@ -27,50 +32,30 @@ func run(m *testing.M) (int, error) {
 	if err != nil {
 		return -1, fmt.Errorf("could not connect to database: %w", err)
 	}
-
 	defer func() {
 		db.Exec("DROP TABLE bookmarks")
 		db.Close()
 	}()
-
 	s = &Storage{db: db}
-
 	return m.Run(), nil
 }
 
 func TestConnection(t *testing.T) {
 	if err := s.db.Ping(); err != nil {
-		t.Errorf("database connection failed: %w", err)
+		t.Errorf("database connection failed: %v", err)
 	}
 }
 
 func TestAddBookmark(t *testing.T) {
 
-	tb := []struct {
-		bookmark Bookmark
-		want     int64
-	}{
-		{Bookmark{name: "google", url: "google.com"}, 1},
-		{Bookmark{name: "reddit", url: "reddit.com"}, 2},
-	}
-
 	t.Run("Add bookmark", func(t *testing.T) {
 
-		for _, tt := range tb {
-			got, err := s.AddBookmark(&tt.bookmark)
-			if err != nil {
-				t.Errorf("AddBookmark failed: %v", err)
-			}
-			if got != tt.want {
-				t.Errorf("got %q, want %q", got, tt.want)
-			}
-
-			t.Cleanup(func() {
-				if err = s.DeleteAllBookmarks(); err != nil {
-					t.Fatalf("cleanup failed: %v", err)
-				}
-			})
+		got, err := s.AddBookmark(&testBookmark)
+		if err != nil {
+			t.Errorf("unexpected error in AddBookmark: %v", err)
 		}
+		assertEquals(t, got, 1)
+		teardown(t)
 	})
 }
 
@@ -78,45 +63,186 @@ func TestGetAllBookmarks(t *testing.T) {
 
 	t.Run("Get all bookmarks", func(t *testing.T) {
 
-		tb := []Bookmark{
-			Bookmark{name: "google", url: "google.com"},
-			Bookmark{name: "reddit", url: "reddit.com"},
-		}
-		for _, tt := range tb {
-			_, err := s.AddBookmark(&tt)
-			if err != nil {
-				t.Errorf("setup failed: %v", err)
-			}
-		}
-
+		seedMultiple(t)
 		bList, err := s.GetAllBookmarks()
 		if err != nil {
 			t.Fatalf("GetAllBookmarks failed: %v", err)
 		}
-		if len(bList) != len(tb) {
-			t.Errorf("got %q, want %q", len(bList), len(tb))
-		}
 
-		t.Cleanup(func() {
-			if err = s.DeleteAllBookmarks(); err != nil {
-				t.Fatalf("cleanup failed: %v", err)
-			}
-		})
+		assertEquals(t, len(bList), len(testList))
+		for i := range bList {
+			assertBookmarkEquals(t, bList[i], testList[i])
+		}
+		teardown(t)
 	})
 }
 
 func TestGetBookmarkById(t *testing.T) {
 
+	t.Run("get existing bookmark by id", func(t *testing.T) {
+		id := seedOne(t)
+		got, err := s.GetBookmarkById(id)
+		if err != nil {
+			t.Fatalf("GetBookmarkById failed: %v", err)
+		}
+		want := testBookmark
+		assertBookmarkEquals(t, got, want)
+		teardown(t)
+	})
+
+	t.Run("get non-existent bookmark", func(t *testing.T) {
+		got, err := s.GetBookmarkById(1000)
+		if err != nil {
+			t.Fatalf("GetBookmarkById failed: %v", err)
+		}
+		want := Bookmark{}
+		if got != want {
+			t.Fatalf("non-existent bookmark found: %q", got)
+		}
+	})
 }
 
 func TestDeleteBookmark(t *testing.T) {
+	t.Run("delete bookmark", func(t *testing.T) {
+		id := seedOne(t)
+		rows, err := s.DeleteBookmark(id)
+		if err != nil {
+			t.Fatalf("DeleteBookmark failed: %v", err)
+		}
+		if rows != 1 {
+			t.Fatalf("wrong number of bookmarks deleted: %d/1", rows)
+		}
+		teardown(t)
+	})
 
+	t.Run("delete 1 bookmark only out of 2", func(t *testing.T) {
+		idList := seedMultiple(t)
+		rows, err := s.DeleteBookmark(idList[0])
+		if err != nil {
+			t.Fatalf("DeleteBookmark failed: %v", err)
+		}
+		if rows != 1 {
+			t.Fatalf("wrong number of bookmarks deleted: %d/1", rows)
+		}
+		bList, err := s.GetAllBookmarks()
+		if err != nil {
+			t.Errorf("get bookmarks failed: %v", err)
+		}
+		if len(bList) != len(idList)-1 {
+			t.Fatalf("wrong number of bookmarks remaining: %d/%d", len(bList), len(idList)-1)
+		}
+		teardown(t)
+	})
+
+	t.Run("delete non-existent bookmark", func(t *testing.T) {
+		rows, err := s.DeleteBookmark(1000)
+		if err != nil {
+			t.Fatalf("DeleteBookmark failed: %v", err)
+		}
+		if rows != 0 {
+			t.Fatalf("wrong number of bookmarks deleted: %d/0", rows)
+		}
+		teardown(t)
+	})
 }
 
 func TestDeleteAllBookmarks(t *testing.T) {
 
+	idList := seedMultiple(t)
+	rows, err := s.DeleteAllBookmarks()
+	if err != nil {
+		t.Fatalf("DeleteAllBookmarks failed: %v", err)
+	}
+	if int(rows) != len(idList) {
+		t.Fatalf("wrong number of bookmarks deleted: %d/%d", int(rows), len(idList))
+	}
+	bList, err := s.GetAllBookmarks()
+	if err != nil {
+		t.Errorf("get bookmarks failed: %v", err)
+	}
+	if len(bList) != 0 {
+		t.Fatalf("wrong number of bookmarks remaining: %d/0", len(bList))
+	}
+	teardown(t)
 }
 
 func TestUpdateBookmark(t *testing.T) {
 
+	id := seedOne(t)
+	newTestBookmark := &Bookmark{name: "google search", url: "google.com"}
+	rows, err := s.UpdateBookmark(id, newTestBookmark)
+	if err != nil {
+		t.Fatalf("UpdateBookmark failed: %v", err)
+	}
+	if rows != 1 {
+		t.Fatalf("wrong number of bookmarks updated: %d/1", rows)
+	}
+	b, err := s.GetBookmarkById(id)
+	if err != nil {
+		t.Errorf("get bookmarks failed: %v", err)
+	}
+	assertBookmarkEquals(t, b, *newTestBookmark)
+	teardown(t)
+}
+
+func TestGetBaseDir(t *testing.T) {
+
+	t.Run("linux", func(t *testing.T) {
+		got, err := GetBaseDir()
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		want := "/home/kenc/.local/share"
+		if got != want {
+			t.Fatalf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func seedOne(t testing.TB) int {
+	t.Helper()
+	id, err := s.AddBookmark(&testBookmark)
+	if err != nil {
+		t.Errorf("seed database failed: %v", err)
+	}
+	return id
+}
+
+func seedMultiple(t testing.TB) []int {
+	t.Helper()
+	var idList []int
+	for _, tt := range testList {
+		id, err := s.AddBookmark(&tt)
+		if err != nil {
+			t.Errorf("seed database failed: %v", err)
+		}
+		idList = append(idList, id)
+	}
+	return idList
+}
+
+func teardown(t testing.TB) {
+	t.Helper()
+	t.Cleanup(func() {
+		if _, err := s.DeleteAllBookmarks(); err != nil {
+			t.Fatalf("cleanup failed: %v", err)
+		}
+	})
+}
+
+func assertEquals(t testing.TB, got int, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertBookmarkEquals(t testing.TB, got Bookmark, want Bookmark) {
+	t.Helper()
+	if got.name != want.name {
+		t.Errorf("got %q, want %q", got.name, want.name)
+	}
+	if got.url != want.url {
+		t.Errorf("got %q, want %q", got.url, want.url)
+	}
 }
